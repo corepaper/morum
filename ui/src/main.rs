a4ad4165874eeeb4d758265003bc09180d7b35a2
Wei Tang <wei@that.world>
2022-11-19 08:48:33 +0100

    Posts display UI

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -55,12 +55,14 @@ impl Reducible for PersistedValue {
 
 type Persisted = UseReducerHandle<PersistedValue>;
 
-#[derive(Debug, Clone, Copy, PartialEq, Routable)]
+#[derive(Debug, Clone, PartialEq, Routable)]
 enum Route {
     #[at("/")]
     Home,
     #[at("/login")]
     Login,
+    #[at("/category/:category_id")]
+    Posts { category_id: String },
     #[not_found]
     #[at("/404")]
     NotFound,
@@ -71,6 +73,7 @@ fn switch(routes: Route) -> Html {
         Route::Home => html! { <Categories /> },
         Route::NotFound => html! { <h1>{ "404" }</h1> },
         Route::Login => html! { <Login /> },
+        Route::Posts { category_id } => html! { <Posts category_id={category_id} /> },
     }
 }
 
@@ -290,11 +293,101 @@ fn Subcategory(props: &SubcategoryProps) -> Html {
         <div class="col-sm-6">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title"><a href="#">{props.subcategory.title.clone()}</a></h5>
+                    <h5 class="card-title"><Link<Route> to={Route::Posts { category_id: props.subcategory.id.clone().unwrap_or("uncategorized".to_string()) }}>{props.subcategory.title.clone()}</Link<Route>></h5>
                     <p class="card-text">{props.subcategory.topic.clone()}</p>
                 </div>
             </div>
         </div>
+    }
+}
+
+#[derive(Properties, PartialEq, Clone)]
+struct PostsProps {
+    pub category_id: String,
+}
+
+#[function_component]
+fn Posts(props: &PostsProps) -> Html {
+    let persisted = use_context::<Persisted>().expect("no ctx found");
+    let navigator = use_navigator().unwrap();
+
+    let fetched = use_state::<Option<(types::Category, types::Subcategory, Vec<types::Post>)>, _>(|| None);
+
+    if let Some((category, subcategory, posts)) = (*fetched).clone() {
+        html! {
+            <>
+                <div class="row mb-3">
+                    <h3>
+                        {category.title.clone()}{" > "}{subcategory.title.clone()}
+                        <small>{subcategory.topic.clone()}</small>
+                    </h3>
+                </div>
+                { posts.iter().map(|post| html! {
+                    <div class="row">
+                        <div class="card">
+                            <h5 class="card-title"><a href="#">{post.title.clone()}</a></h5>
+                            if let Some(topic) = post.topic.clone() {
+                                <p class="card-text">{topic}</p>
+                            }
+                            <hr />
+                        </div>
+                    </div>
+                }).collect::<Html>() }
+            </>
+        }
+    } else {
+        let fetched = fetched.clone();
+        let props = props.clone();
+
+        yew::platform::spawn_local(async move {
+            let res = Request::get(&(API_PREFIX.to_owned() + "/api/native/categories"))
+                .send()
+                .await
+                .unwrap()
+                .json::<params::CategoriesResponse>()
+                .await
+                .unwrap();
+
+            let mut category = None;
+            let mut subcategory = None;
+
+            for c in res.categories {
+                for sc in c.subcategories.clone() {
+                    if sc.id == Some(props.category_id.clone()) || (sc.id == None && props.category_id == "uncategorized") {
+                        category = Some(c.clone());
+                        subcategory = Some(sc.clone());
+                    }
+                }
+            }
+
+            let category = if let Some(category) = category {
+                category
+            } else {
+                navigator.push(&Route::Home);
+                return;
+            };
+
+            let subcategory = if let Some(subcategory) = subcategory {
+                subcategory
+            } else {
+                navigator.push(&Route::Home);
+                return;
+            };
+
+            let res = Request::get(&(API_PREFIX.to_owned() + &format!("/api/native/posts?category_id={}", props.category_id)))
+                .send()
+                .await
+                .unwrap()
+                .json::<params::PostsResponse>()
+                .await
+                .unwrap();
+
+            fetched.set(Some((category, subcategory, res.posts)));
+        });
+
+        html! {
+            <p>{"Loading ..."}</p>
+        }
     }
 }
 
